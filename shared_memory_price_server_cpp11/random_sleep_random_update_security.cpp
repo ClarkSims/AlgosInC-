5e87462a4722c399e6223d371b157524fbf91770
Clark Sims <clark.n.sims@gmail.com>
2019-06-03 09:37:28 -0400

    compile link

--- shared_memory_price_server_cpp11/random_sleep_random_update_security.cpp
+++ shared_memory_price_server_cpp11/random_sleep_random_update_security.cpp
@@ -11,14 +11,31 @@ size_t random_sleep_random_update_security(volatile security_datum* sec_datum, s
         size_t sec_off = random() % num_sec_datum;
         size_t ex_off = random() % (NUM_STOCK_EXCHANGE - 1) + 1; // skip nbbo
         volatile security_datum* nbbo_update = &sec_datum[sec_off*NUM_STOCK_EXCHANGE];
-        // ugly fix this
+
+        // ugly! fix this, did this because I was getting compile errors
+        // used excessive force to insure against reordering
         price_datum* nbbo_pd = (price_datum*) ((security_datum*) nbbo_update)->get_older();
         nbbo_pd->mark_dirty();
+        atomic_signal_fence(std::memory_order_acq_rel);
 
         volatile security_datum* update = &sec_datum[sec_off*NUM_STOCK_EXCHANGE + ex_off];
-        size_t tick_size = random() % 7 - 3;
+        // ugly! fix this, did this because I was getting compile errors
+        // used excessive force to insure against reordering
         price_datum* pd = (price_datum*) ((security_datum*) nbbo_update)->get_older();
         pd->mark_dirty();
+        atomic_signal_fence(std::memory_order_acq_rel);
+
+        size_t tick_size = random() % 7 - 3;
+        size_t bo_spread = random() %5 + 5;
+        pd->bid += tick_size;
+        pd->bid_size = random() % 7 + 1;
+        pd->ask = pd->bid + bo_spread;
+
+        //std::vector<int
+        if (pd->bid > nbbo_pd->ask) {
+
+        }
+
 
     }
     return 1;

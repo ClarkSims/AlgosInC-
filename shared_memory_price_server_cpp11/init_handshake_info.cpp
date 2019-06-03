dabc6aa47abbbd4eb7c8f1b0022d1b637264f1b8
Clark Sims <clark.n.sims@gmail.com>
2019-06-03 04:50:10 -0400

    compile and link shared_memory_price_server

--- shared_memory_price_server_cpp11/init_handshake_info.cpp
+++ shared_memory_price_server_cpp11/init_handshake_info.cpp
@@ -2,22 +2,39 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <stdlib.h>
+#include <assert.h>
 
 #include "shared_memory_price_server.h"
+
+unsigned num_stock_symbols;
+size_t security_encoding_shmem_size = 16384*sizeof(security_encoding)*NUM_STOCK_EXCHANGE;
 
 // populates shared memory file with list of exchanges, tickers, security ids, and tick denominators
 // ie security encodings
 void init_handshake_info(security_encoding*& sec_codes, size_t& num_sec_codes) {
-    size_t i;
+    size_t i, j;
 
+    num_stock_symbols = 0;
+    for (i=0; tickers[i][0]!=0; ++i) {}
+    num_stock_symbols = i;
+
+    size_t min_shmem_size = num_stock_symbols * NUM_STOCK_EXCHANGE * sizeof(security_encoding);
+    assert(min_shmem_size < security_encoding_shmem_size);
+
+
+    for (i=0; tickers[i][0]!=0; ++i) {
+        for (j=0; exchanges[j]!=0; ++j) {
+
+        }
+    }
 }
 
-const char *exchanges[] = {
+const char *exchanges[NUM_STOCK_EXCHANGE] = {
+    "NBBO",
     "ARCA",
     "BATS",
     "NASDAQ",
     "NYSE"
-    ""
 };
 
 const char *tickers[] = {
--- shared_memory_price_server_cpp11/shared_memory_price_server.h
+++ shared_memory_price_server_cpp11/shared_memory_price_server.h
@@ -1,12 +1,16 @@
 #ifndef SHARED_MEMORY_PRICE_SERVER_H
 #define SHARED_MEMORY_PRICE_SERVER_H
 
+#define NUM_STOCK_EXCHANGE 5 
 #define SHARED_MEMORY_PRICE_SERVER_CPU_AFFINITY 3
 #define HANDSHAKE_ID 1234
 #define HANDSHAKE_SIZE 65536
 #define CIRCULAR_QUEUE_ID 2345
 
+
 #include <stdint.h>
+#include <assert.h>
+#include <string.h>
 
 void set_cpu_affinity(int num);
 
@@ -17,11 +21,32 @@ void termination_handler (int signum);
 void set_signal_handlers(volatile int * flag);
 
 struct security_encoding {
-    char ticker[32];
+    char ticker[8];
     char exchange[8];
     uint32_t id;
     uint32_t denominator;
+    char padding[8];
+
+    security_encoding() {
+        memset( this, 0, sizeof(*this));
+    }
+
+    security_encoding(
+        const char *Ticker,
+        const char *Exchange,
+        uint32_t Id=0,
+        uint32_t Denom=100) 
+    {
+        assert( strlen(Ticker) < sizeof(ticker));
+        assert( strlen(Exchange) < sizeof(exchange));
+    }
 };
+
+extern unsigned num_stock_symbols;
+extern size_t security_encoding_shmem_size;
+extern const char *exchanges[NUM_STOCK_EXCHANGE];
+extern const char *tickers[];
+
 
 // populates shared memory file with list of exchanges, tickers, security ids, and tick denominators
 // ie security encodings

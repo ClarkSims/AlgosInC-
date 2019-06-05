3eddc4ec333bddc2db4410d2a62d72992890f6df
Clark Sims <clark.n.sims@gmail.com>
2019-06-05 12:32:53 -0400

    added build log, makefiles

---
+++ circular_queue_cpp11/test/regtest1_circular_queue_in_shared_memory.sh
@@ -0,0 +1,14 @@
+#!/../bin/bash
+
+### Testing read write between processes using a circular queue in shared memory
+
+echo "running test1_circular_queue_in_shared_memory parent";
+../../bin/test1_circular_queue_in_shared_memory parent &
+sleep 1
+
+echo "running test1_circular_queue_in_shared_memory child";
+../../bin/test1_circular_queue_in_shared_memory child || { echo "../../bin/test1_circular_queue_in_shared_memory failed"; exit 1; }
+
+echo "running test1_circular_queue_in_shared_memory cleanup";
+../../bin/test1_circular_queue_in_shared_memory cleanup || { echo "../../bin/test1_circular_queue_in_shared_memory failed"; exit 1; }
+
